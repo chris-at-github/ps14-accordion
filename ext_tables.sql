165b8b68dea730d8afec8844965c908ab613b5bc
Christian Pschorr <pschorr.christian@gmail.com>
2024-04-05 17:59:55 +0200

    Anpassung Tabelle in Kommentar

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,5 +1,5 @@
 #
-# Table structure for table 'tx_xo_domain_model_elements'
+# Table structure for table 'tx_foundation_domain_model_elements'
 #
 CREATE TABLE tx_foundation_domain_model_elements (
 	tx_accordion_active smallint(5) unsigned DEFAULT '0' NOT NULL
